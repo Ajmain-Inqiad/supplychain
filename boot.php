d247602f3600b19021e869b04e6c218908e50d97
Ajmain Inqiad <ajmaininqiadalam@gmail.com>
2018-06-05 02:29:57 +0600

    Delete boot.php

--- boot.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "vendor/autoload.php";
-Braintree_Configuration::environment('sandbox');
-Braintree_Configuration::merchantId('gkqtg979h3s7zsq5');
-Braintree_Configuration::publicKey('vnhtvmdp4fwcw34c');
-Braintree_Configuration::privateKey('95e46ef52498531133af9e6969a8d482');
-
-?>
