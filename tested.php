82d89d4635b41ecf49a00e6aeaeecb0c52557d8f
Ajmain Inqiad <ajmaininqiadalam@gmail.com>
2018-06-05 02:33:52 +0600

    Delete tested.php

--- tested.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lat = [23.780527, 23.780330, 23.780341, 23.780346];
-$long = [90.417385, 90.416284, 90.416025, 90.414847];
-for($i = 0; $i<sizeof($lat); $i++){
-    $url = "https://api.thingspeak.com/update?api_key=DYHXEIQHNADFA68L&field1="+$lat[$i]+"&field2="+$long[$i];
-}
-
- ?>
